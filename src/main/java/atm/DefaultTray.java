aabfd5951414721a7f181cd8b3211b033248699d
booyakashakawabangha <tatarik.sv@gmail.com>
2021-12-09 22:40:33 +0200

    Initial commit

---
+++ src/main/java/atm/DefaultTray.java
@@ -0,0 +1,21 @@
+package atm;
+
+public abstract class DefaultTray implements Tray{
+    private final int bill;
+    private Tray nextTray;
+
+    public DefaultTray(int bill){
+        this.bill = bill;
+    }
+
+    public void process (int amount){
+        System.out.println("Extracted " + amount / bill + " bills with nominal of " + bill);
+        if (nextTray != null){
+            nextTray.process(amount % bill);
+        }
+    }
+
+    public void setNext(Tray tray){
+        this.nextTray = tray;
+    }
+}
---
+++ src/main/java/atm/Tray50.java
@@ -0,0 +1,7 @@
+package atm;
+
+public class Tray50 extends DefaultTray{
+    public Tray50(){
+        super(50);
+    }
+}
